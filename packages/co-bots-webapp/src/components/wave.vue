555e78bc5ca141c6522ac0c492d742d5b8863430
Matthieu Auger <matthieua@theodo.fr>
2022-04-26 00:30:38 +0200

    Fix home

---
+++ packages/co-bots-webapp/src/components/wave.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-[32px] bg-[url('../images/Waves.svg')]">
+    <img src="../images/Waves.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'wave'
+}
+</script>
--- packages/co-bots-webapp/src/views/Home.vue
+++ packages/co-bots-webapp/src/views/Home.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="overflow-hidden overscroll-none">
-    <div class="flex flex-col justify-center items-center">
-      <img
-        class="hidden lg:block w-full mt-[72px] max-w-3xl p-6"
-        src="../images/ExtravagainzaLabel.png"
-      />
-
-      <div class="w-[200px] h-1 bg-cobots-silver rounded"></div>
-
-      <div class="font-['CheeseButterCream'] mb-4 mt-11 px-6 text-black text-3xl">Hey Anon, wanna play a game?</div>
-      <div class="w-[675px] text-cobots-silver-3 text-xl px-6">It's simple: at each mint checkpoint, a Co-Bot wins a prize. The earlier you mint, the more chances you have to win. Good luck!</div>
-    </div>
-    <div
-      class="flex flex-col justify-center items-center my-12"
-      :class="{
-        '':
-          canFlip || canMint || mintFailed || refundEnabled,
-      }"
-    >
-      <div class="w-96 bg-cobots-silver rounded-3xl p-4 flex flex-col h-[248px]">
-        <div class="text-2xl text-center font-extrabold text-cobots-silver-3">MINT CO-BOTS</div>
-        <connect-wallet-panel
-          v-if="!walletConnected && !canFlip && (canMint || refundEnabled)"
+    <div class="bg-white pb-10">
+      <div class="flex flex-col justify-center items-center">
+        <img
+          class="hidden lg:block w-full mt-[72px] max-w-3xl p-6"
+          src="../images/ExtravagainzaLabel.png"
         />
-        <mint-panel v-else-if="canMint" />
-        <div class="flex-grow" v-else-if="mintFailed"></div>
-        <bonus-challenge-panel
-          v-else-if="canFlip"
-          @moreDetailsClick="scrollToBonusPrizes"
-        />
-        <raffle v-else />
+
+        <div class="w-[200px] h-1 bg-cobots-silver rounded"></div>
+
+        <div class="font-['CheeseButterCream'] mb-4 mt-11 px-6 text-black text-3xl">Hey Anon, wanna play a game?</div>
+        <div class="w-[675px] text-cobots-silver-3 text-xl px-6">It's simple: at each mint checkpoint, a Co-Bot wins a prize. The earlier you mint, the more chances you have to win. Good luck!</div>
+      </div>
+      <div
+        class="flex flex-col justify-center items-center my-12"
+        :class="{
+          '':
+            canFlip || canMint || mintFailed || refundEnabled,
+        }"
+      >
+        <div class="w-96 bg-cobots-silver rounded-3xl p-4 flex flex-col h-[248px]">
+          <div class="text-2xl text-center font-extrabold text-cobots-silver-3">MINT CO-BOTS</div>
+          <connect-wallet-panel
+            v-if="!walletConnected && !canFlip && (canMint || refundEnabled)"
+          />
+          <mint-panel v-else-if="canMint" />
+          <div class="flex-grow" v-else-if="mintFailed"></div>
+          <bonus-challenge-panel
+            v-else-if="canFlip"
+            @moreDetailsClick="scrollToBonusPrizes"
+          />
+          <raffle v-else />
+        </div>
       </div>
     </div>
+
 
     <div class="bg-black text-white flex flex-col items-center pb-32">
       <div class="flex flex-col mt-32 text-[80px] leading-[80px] font-['CheeseButterCream'] items-center">
@@ -99,6 +102,7 @@ import Raffle from "@/components/Raffle/index.vue";
 import Refund from "@/components/Refund.vue";
 import GaugeStack from "@/components/GaugeStack.vue";
 import GaugeSeparator from "@/components/GaugeSeparator.vue";
+import Wave from "@/components/wave.vue";
 
 export default {
   name: "Home",
@@ -109,7 +113,8 @@ export default {
     Raffle,
     Refund,
     GaugeStack,
-    GaugeSeparator
+    GaugeSeparator,
+    Wave
   },
   data: () => ({
     interval: null,
